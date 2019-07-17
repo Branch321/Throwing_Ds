4184a88cb43bb42079b6a4051edac2530e66bae9
Branch321 <tythatsk8s05@yahoo.com>
2019-07-17 18:19:14 -0500

    Corrected behavior of modifier parsing.

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ import random
 
 #TODO: Add this to Discord for friends to use
 
-
 # Purpose: Function that parses and sanitizes user input
-# Post: Returns a dictionary of dice options format {# sided dice: [# of rolls, modifier]}
+# Post: Returns a dictionary of dice options format: {# sided dice: [# of rolls, modifier]}
 def parse_down(dice_list):
     dice_dictionary = {}
     for each_dice in dice_list:
         split_dice = each_dice.split('d')
-        modifier_position = 0
         if '-' in split_dice[1]:
             modifier_position = split_dice[1].find('-')
             modifier = split_dice[1][modifier_position:len(split_dice[1])]
-        if '+' in split_dice[1]:
+            dice_dictionary.update({split_dice[1][:modifier_position]: [split_dice[0], modifier]})
+        elif '+' in split_dice[1]:
             modifier_position = split_dice[1].find('+')
             modifier = split_dice[1][modifier_position:len(split_dice[1])]
-        dice_dictionary.update({split_dice[1][:modifier_position] : [split_dice[0],modifier]})
+            dice_dictionary.update({split_dice[1][:modifier_position]: [split_dice[0], modifier]})
+        else:
+            modifier = '0'
+            dice_dictionary.update({split_dice[1]: [split_dice[0],modifier]})
     print(dice_dictionary)
     return dice_dictionary
 
@@ -30,7 +32,7 @@ def random_dice_generator(dice_dictionary):
     print(dice_dictionary)
     actual_rolls = []
     for dice in dice_dictionary.keys():
-        for number in range(0,int(dice_dictionary[dice])):
+        for number in range(0,int(dice_dictionary[dice][0])):
             actual_rolls.append(random.randint(1,int(dice)))
     print(actual_rolls)
     print(max(actual_rolls))
@@ -38,7 +40,7 @@ def random_dice_generator(dice_dictionary):
 while True:
     #TODO: Add stat/skill specific rolls
     #Get input from user
-    dice_roll = input("How many to roll? (format: 1d10 or initiative)  ")
+    dice_roll = input("How many to roll? (format: 1d10-2 or initiative)  ")
 
     #Below is the initiative roll (Always 1d20)
     if dice_roll == "init":
@@ -47,5 +49,5 @@ while True:
     else:
         dice_roll = dice_roll.split(' ')
         dice_options = parse_down(dice_roll)
-#        random_dice_generator(dice_options)
+        random_dice_generator(dice_options)
 
