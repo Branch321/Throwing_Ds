56535dd96369560ae7652852e6f7143ef807f140
Branch321 <tythatsk8s05@yahoo.com>
2019-07-17 18:03:00 -0500

    Working on how to deal with modifier. Finished the parse down of it.

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import random
 #TODO: Add this to Discord for friends to use
 
 
-# Purpose: Write a function to parse down the code
-# Post: Returns a dictionary of dice options (key is # of sides on dice, value is number of rolls)
+# Purpose: Function that parses and sanitizes user input
+# Post: Returns a dictionary of dice options format {# sided dice: [# of rolls, modifier]}
 def parse_down(dice_list):
-
-    #dice_list = ['1d5'] #DELETE AFTER DEBUG
     dice_dictionary = {}
     for each_dice in dice_list:
         split_dice = each_dice.split('d')
-        dice_dictionary.update({split_dice[1] : split_dice[0]})
+        modifier_position = 0
+        if '-' in split_dice[1]:
+            modifier_position = split_dice[1].find('-')
+            modifier = split_dice[1][modifier_position:len(split_dice[1])]
+        if '+' in split_dice[1]:
+            modifier_position = split_dice[1].find('+')
+            modifier = split_dice[1][modifier_position:len(split_dice[1])]
+        dice_dictionary.update({split_dice[1][:modifier_position] : [split_dice[0],modifier]})
+    print(dice_dictionary)
     return dice_dictionary
 
 # Purpose: Randomizes the dice rolls and prints the max of the rolls
@@ -41,5 +47,5 @@ while True:
     else:
         dice_roll = dice_roll.split(' ')
         dice_options = parse_down(dice_roll)
-        random_dice_generator(dice_options)
+#        random_dice_generator(dice_options)
 
