a48ddce20ad438f7db4c0e9b492fb14c543d5939
Branch321 <tythatsk8s05@yahoo.com>
2019-07-19 15:24:47 -0500

    Deleted old print statements.

--- main.py
+++ main.py
@@ -28,11 +28,9 @@ def parse_down(dice_list, damage=False):
     # if no external modifier default to 0
     else:
         dice_dictionary = {'modifier': 0}
-    print(dice_dictionary)
     # Below is default 1d6 if it is not a damage roll
     if not damage:
         dice_dictionary['6'] = '1'
-    print(dice_dictionary)
     # parse and form the dictionary to return
     for each_dice in dice_list:
         # condition for dice roll
