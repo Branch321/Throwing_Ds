6d4ad2dd2403ee90971580fbdcc88fafc51ee471
Branch321 <tythatsk8s05@yahoo.com>
2019-07-19 20:16:39 -0500

    Changed to a woman's voice.

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import player
 import pyttsx3
 
 engine = pyttsx3.init()
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
 engine.say("I will speak this text")
 engine.runAndWait()
 
